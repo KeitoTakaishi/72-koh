e1d706c04f943fac35147ea1944789a80a86dbcf
takaishi keito <keitoncis@icloud.com>
2018-09-28 17:40:37 +0900

    model update

---
+++ Assets/Scripts/rePlace.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rePlace : MonoBehaviour {
+    List<GameObject> child;
+
+
+	void Start () {
+        child = new List<GameObject>();
+        var num = transform.childCount;
+        Vector3 ParentPos = this.transform.position;
+        float step = 6.0f;
+
+        if (num == 3)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                child.Add(transform.GetChild(i).gameObject);
+                Debug.Log(child[i]);
+                child[i].transform.position = new Vector3(ParentPos.x, ParentPos.y - (i-1.0f) * step, ParentPos.z);
+            }
+        }else{
+            for (int i = 0; i < num; i++)
+            {
+                child.Add(transform.GetChild(i).gameObject);
+                Debug.Log(child[i]);
+                child[i].transform.position = new Vector3(ParentPos.x, ParentPos.y - (i-1.5f) * step, ParentPos.z);
+            }
+        }
+
+
+	}
+	
+	void Update () {
+		
+	}
+}
