1db3c649bae6beb02cbe4d707962578c9d5936fe
takaishi keito <keitoncis@icloud.com>
2018-10-03 02:04:33 +0900

    update

--- Assets/Scripts/CreateCSV.cs
+++ Assets/Scripts/CreateCSV.cs
@@ -16,19 +16,20 @@ namespace MainScene
         void Start()
         {
             //存在してない場合のみ生成
-            if (!(System.IO.File.Exists(Application.dataPath + "/Resources/CSV/CSVData.csv")))
+            if (!(System.IO.File.Exists(Application.dataPath + "/Resources/CSV/ModifyData.csv")))
             {
-                ReadCSV(ref data);
-                WriteCSV(ref data);
+                ReadCSV(ref data, "temp");//date,temp
+                WriteCSV(ref data);//year;temp
+            }else{
+                Debug.Log("exist");
             }
         }
 
 
         #region ReadCSV
-        public void ReadCSV(ref List<string> data)
+        public void ReadCSV(ref List<string> data, string FileName)
         {
-            var fileName = "temp";
-            var csvFile = Resources.Load("csv/" + fileName) as TextAsset;
+            var csvFile = Resources.Load("csv/" + FileName) as TextAsset;
             var reader = new StringReader(csvFile.text);
 
             while (reader.Peek() > -1)
@@ -42,17 +43,32 @@ namespace MainScene
         }
         #endregion
 
+        #region LoadCSV
+        public void LoadFile(ref List<string> data, string FileName)
+        {
+            var csvFile = Resources.Load("csv/" + FileName) as TextAsset;
+            var reader = new StringReader(csvFile.text);
+            while (reader.Peek() > -1)
+            {
+                string lineData = reader.ReadLine();
+                data.Add(lineData);
+            }
+        }
+        #endregion
+
         #region WriteCSV
         private void WriteCSV(ref List<string> data)
         {
             StreamWriter sw;
             FileInfo fi;
             string str = null;
-            for (int i = 0; i < data.Count / 3; i += 4)
+            for (int i = 0; i < data.Count; i+=2)
             {
-                str += "DATA:" + data[i] + " : " + data[i + 1] + "\n";
+                //date + temperature
+                data[i] = data[i].Split('/')[0]; 
+                str += data[i] + ":" + data[i + 1] + "\n";
             }
-            fi = new FileInfo(Application.dataPath + "/Resources/CSV/CSVData.csv");
+            fi = new FileInfo(Application.dataPath + "/Resources/CSV/ModifyData.csv");
             sw = fi.AppendText();
             sw.WriteLine(str);
             sw.Flush();
--- Assets/Scripts/GenerateMesh.cs
+++ Assets/Scripts/GenerateMesh.cs
@@ -7,90 +7,123 @@ namespace MainScene
 {
     public class GenerateMesh : MonoBehaviour
     {
-        private List<string> tempData;
+        private List<string> ModifyData;
+        private int[] CycleData;
         private CreateCSV _CreateCSV;
+        private const int num = 72;
+        List<float>[] tempData;
+
+
         private Mesh mesh;
         private MeshFilter mf;
 
+
+
         void Start()
         {
-            tempData = new List<string>();
-            _CreateCSV = GameObject.Find("CSV").GetComponent<CreateCSV>();
-            _CreateCSV.ReadCSV(ref tempData);
-            ModifyData(ref tempData);
+            //cycle日数をセット
+            CycleData = new int[num];
+            InitCycleData(ref CycleData);
 
+            ModifyData = new List<string>();
+            //CSVData -> Year:temperature
+            _CreateCSV = GameObject.Find("CSV").GetComponent<CreateCSV>();
+            _CreateCSV.LoadFile(ref ModifyData, "ModifyData");
+
+
+
+            tempData = new List<float>[num];
+            for (int i = 0; i < num; i++){
+                tempData[i] = new List<float>();
+            }
+
+            SevrntyTwoDataBase(ModifyData, ref tempData);
+
+            /*
             mesh = new Mesh();
             mesh = CreateMesh(ref mesh, tempData);
             mf = GetComponent<MeshFilter>();
             mf.sharedMesh = mesh;
             mf.mesh.SetIndices(mf.mesh.GetIndices(0), MeshTopology.LineStrip, 0);
+            */
         }
 
         void Update()
         {
-            this.transform.position += new Vector3(Time.deltaTime*4.0f, 0.0f, 0.0f);
+            //this.transform.position += new Vector3(Time.deltaTime*4.0f, 0.0f, 0.0f);
         }
 
-        private void ModifyData(ref List<string>data){
-            string year,month,_date;
 
-            string date;
-            int dateCount = 0;
-            int CurSeason = 0;
-            const int num = 72;
-            List<float>[] temp = new List<float>[num];
+		private void InitCycleData(ref int[] dates)
+		{
+            for (int i = 0; i < 72; i++){
+                dates[i] = 5;
+            }
+            dates[0] =  4;
+            dates[27] = 6;
+            dates[30] = 6;
+            dates[34] = 6;
+            dates[39] = 6;
+            dates[40] = 6;
+            dates[42] = 4;
+            dates[43] = 6;
+            dates[48] = 6;
+            dates[68] = 4;
+            dates[69] = 6;
+		}
 
 
-            for (int i = 0; i < data.Count; i+=2){
-                year = data[i].Split('/')[0];
-                month = data[i].Split('/')[1];
-                _date = data[i].Split('/')[2];
+        //csvから72候型のデータを作成
+        private void SevrntyTwoDataBase(List<string> data, ref List<float>[] temp){
+            int CurSeason;//71まで回ったら初期化
+            int CurDate;//3
+            CurSeason = 0;
+            CurDate = 0;
 
-                //うるう年の振り分け
-                if(int.Parse(year) % 4 == 0){
-                    
-                    if (dateCount == 366)
+
+            Debug.Log("count;" + data.Count);
+
+            for (int i = 0; i < data.Count;)
+            {
+                if (int.Parse(data[i].Split(':')[0]) % 4 != 0)
+                {
+                    for (int j = 0; j < CycleData[CurSeason]; j++)
                     {
-                        temp[CurSeason].Add(float.Parse(data[i + 1]));
-                        Debug.Log(data[i]);
-                        dateCount = 0;
+                        var t = float.Parse(data[i].Split(':')[1]);
+                        tempData[CurSeason].Add(t);
+                        i++;
+                    }
+                    CurSeason += 1;
+                    if (CurSeason > 71)
+                    {
                         CurSeason = 0;
                     }
-                    else
-                    {
-                        if (int.Parse(month) != 2 && int.Parse(_date) != 29)
+                    Debug.Log(i);
+                }else{
+                    if (CurSeason != 11){
+                        for (int j = 0; j < CycleData[CurSeason]; j++)
                         {
-                            temp[CurSeason].Add(float.Parse(data[i + 1]));
+                            var t = float.Parse(data[i].Split(':')[1]);
+                            tempData[CurSeason].Add(t);
+                            i++;
+                        }
+                    }else{
+                        for (int j = 0; j < CycleData[CurSeason]+1; j++)
+                        {
+                            var t = float.Parse(data[i].Split(':')[1]);
+                            tempData[CurSeason].Add(t);
+                            i++;
                         }
                     }
-                    if (int.Parse(month) != 2 && int.Parse(_date) != 29)
+                    CurSeason += 1;
+                    if (CurSeason > 71)
                     {
-                        dateCount++;
-                    }
-                    if (dateCount % 5 == 0)
-                    {
-                        CurSeason++;
-                    }
-
-                }else{
-                    
-                    if (dateCount == 365)
-                    {
-                        temp[CurSeason].Add(float.Parse(data[i + 1]));
-                        Debug.Log(data[i]);
-                        dateCount = 0;
                         CurSeason = 0;
-                    }else {
-                        temp[CurSeason].Add(float.Parse(data[i + 1]));
                     }
-                    dateCount++;
-                    if(dateCount % 5 == 0){
-                        CurSeason++; 
-                    }
-
                 }
             }
-        }
+        } 
+
 
         private Mesh CreateMesh(ref Mesh mesh, List<string>data){
             List<Vector3> vertex = new List<Vector3>();
