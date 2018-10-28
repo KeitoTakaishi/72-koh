1a462b5dea597f71c4f9aca492aefc3a21217bd3
takaishi keito <keitoncis@icloud.com>
2018-10-28 18:45:44 +0900

    update scene designe

---
+++ Assets/Camera/Follow.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * targetを追うスクリプト
+ * 
+ */
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using  UnityEngine;
+using Random = System.Random;
+
+
+
+public class Follow : MonoBehaviour
+{
+    #region Nested Classes
+    public enum Interpolator{
+        Lerp,
+        Slerp
+    }
+    #endregion
+
+    #region Editable Properties
+    [SerializeField] Interpolator _interpolator;
+    [SerializeField] Transform _target;
+
+    public Interpolator interpolator
+    {
+        get { return _interpolator; }
+        set { _interpolator = value; }
+    }
+
+    public Transform target
+    {
+        get { return _target; }
+        set { _target = value; }
+    }
+    
+    #endregion
+    
+    private void Update()
+    {
+        if (Time.frameCount % 30 == 1)
+        {
+            var _nextPos = NextPos();
+            var _curPos = this.transform.position;
+            this.transform.position = Vector3.Lerp(_curPos, _nextPos, Time.realtimeSinceStartup);
+        }  
+    }
+
+    private Vector3 NextPos()
+    {
+        var rad = 1.0f;
+        var _nextPos = UnityEngine.Random.insideUnitSphere * rad;
+        return _nextPos;
+    }
+
+    protected virtual void LookAt()
+    {
+        //毎フレームTargetの方向を向くようにする
+        if(target == null) return;
+        this.gameObject.transform.LookAt(target.transform.position);    
+    }
+}
---
+++ Assets/Camera/Switcher.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Switcher: MonoBehaviour
+{
+	[SerializeField] Transform[] _targetList;
+	[SerializeField] float _interval = 5;
+	
+	
+	IEnumerator Start () {
+		var follower = GetComponent<Follow>();
+		yield return null;
+		
+		while (true)
+		{
+			foreach (var target in _targetList)
+			{
+				follower.target = target;
+				yield return new WaitForSeconds(_interval);
+			}
+		}
+	}
+}
